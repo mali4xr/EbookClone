fb610e2c87b73f0a0fd5c8181cc51ee9656c638a
enemali <malitician@gmail.com>
2025-06-05 18:44:09 +0100

    Updated package.json

--- src/components/BookContent.tsx
+++ src/components/BookContent.tsx
@@ -5,7 +5,6 @@ import Controls from './Controls';
 import PageCounter from './PageCounter';
 import InteractiveElements from './InteractiveElements';
 import { QuizModal } from './QuizModal';
-import Narrator from './Narrator';
 
 const BookContent = () => {
   const { 
@@ -99,7 +98,6 @@ const BookContent = () => {
               />
             </div>
             <InteractiveElements page={currentPage} />
-            <Narrator />
           </div>
         </div>
       </div>
