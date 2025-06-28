3062772fd55cbb4da5387cf6ed80255ff87f5515
enemali <malitician@gmail.com>
2025-06-28 13:50:52 +0100

    Updated AIDrawingBook.tsx

--- src/components/AIDrawingBook.tsx
+++ src/components/AIDrawingBook.tsx
@@ -159,7 +159,7 @@ const AIDrawingBook: React.FC<AIDrawingBookProps> = ({ onBack }) => {
         {/* Story Section - Moved here, below history */}
         {showStorySection && (
           <section className="w-full max-w-4xl mx-auto animate__animated animate__fadeInUp">
-            <div className="text-center mb-6">
+            <div className="text-center mb-1">
               <button
                 onClick={generateStory}
                 disabled={isGeneratingStory}
